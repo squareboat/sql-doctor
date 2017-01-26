88a1745a91cbf0016c501daaa72bbd69e7a9239d
Amit Gupta <akaamitgupta@gmail.com>
2017-01-26 22:49:36 +0530

    Updated listen() closure

--- src/SqlDoctor.php
+++ src/SqlDoctor.php
@@ -19,10 +19,10 @@ class SqlDoctor
     {
         $db->connection()->enableQueryLog();
         
-        $event->listen(RequestHandled::class, function ($request, $response) use($db) {
+        $event->listen(RequestHandled::class, function ($handled) use($db) {
             $queries = $db->getQueryLog();
 
-            if ($request->query('sql-doctor') == 2) {
+            if ($handled->request->query('sql-doctor') == 2) {
                 foreach ($queries as $key => $query) {
                     $queries[$key]['query'] = $this->bindValues($query['query'], $query['bindings']);
                 }
